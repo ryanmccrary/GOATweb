ec9310949824e4dffe157863bccd20ad7aa840dc
Chandler Van De Water <chandler.vdw@gmail.com>
2014-07-14 20:00:57 -0400

    progress

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  var mySwiper = $('.swiper-container').swiper({
+    // autoplay: 6000,
+    autoplayDisableOnInteraction: false,
+    speed: 750,
+    mode:'horizontal',
+    loop: true
+  });
+
+});
